f7d4b32c4ca63cb4b9d21984417a987ee2616be1
Sigve Indregard <sigve@indregard.no>
2017-04-12 10:28:07 +0200

    Light bootstrap dashboard

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def nav_li_active?(text, path, icon=nil)
+    content_tag(:li, class: current_page?(path) ? 'active' : 'inactive') do
+
+      link_to path do
+        icon = 'angle-right' unless icon
+        content_tag(:i, nil, class: "pe-7s-#{icon}") + content_tag(:p, text)
+      end
+    end
+  end
 end
