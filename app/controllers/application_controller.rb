313bd26ba859314b1b4b4ad6141f71318de64ef5
Sigve Indregard <sigve@indregard.no>
2017-05-02 11:56:58 +0200

    Style login and password boxes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception, prepend: true
   before_action :authenticate_user!
+
+  layout :pick_layout
+
+  private
+
+    def pick_layout
+      devise_controller? ? 'login' : 'application'
+    end
 end
