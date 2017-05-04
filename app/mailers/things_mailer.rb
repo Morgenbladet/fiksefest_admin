0f155230ee707099a524bfd5064c538c0b0b280a
Sigve Indregard <sigve@indregard.no>
2017-05-04 17:35:59 +0200

    Mailers missing instance variables
    @thing was undefined.

--- app/mailers/things_mailer.rb
+++ app/mailers/things_mailer.rb
@@ -1,10 +1,12 @@
 class ThingsMailer < ApplicationMailer
   def accepted(thing)
+    @thing = thing
     @url = "https://morgenbladet.no/fiksefest#/thing/#{thing.id}"
     mail to: thing.email, subject: "Fiksefest: Din ødelagte ting er publisert"
   end
 
   def submitted(thing)
+    @thing = thing
     mail to: User.pluck(:email), subject: "Ny ting"
   end
 end
